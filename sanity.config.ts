7c7bba474bcdf71f6b25d16e613a42498bb75350
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2024-08-11 13:05:46 +0300

    configure presentation tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,12 @@
-import {defineConfig} from 'sanity'
+import {defineConfig, buildLegacyTheme} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
+import {defaultDocumentNode} from './structure/defaultDocumentNode'
+import {presentationTool} from 'sanity/presentation'
+
+const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
 
 export default defineConfig({
   name: 'default',
@@ -10,8 +15,17 @@ export default defineConfig({
   projectId: '1rw1zmhf',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
-
+  plugins: [
+    structureTool({structure, defaultDocumentNode}),
+    visionTool(),
+    presentationTool({
+      previewUrl: {
+        draftMode: {
+          enable: `${SANITY_STUDIO_PREVIEW_URL}/api/draft-mode/enable`,
+        },
+      },
+    }),
+  ],
   schema: {
     types: schemaTypes,
   },
